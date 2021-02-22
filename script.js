b94fdbb3c9099b28482550a84086d5c6e8aa3180
manongo <74598669+manongo@users.noreply.github.com>
2021-02-22 11:25:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const portfolio = document.querySelector('.grid');
+window.addEventListener('scroll', () =>{
+    if (window.scrollY > 1500 ) {
+        portfolio.classList.add('scroll');
+
+    };
+
+});
+
+
